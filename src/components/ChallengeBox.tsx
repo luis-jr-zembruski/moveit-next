52688769d67347ae1d2ffc42068f93b66f841fbd
luis-jr-zembruski <luis.jr.zembruski@icloud.com>
2021-03-01 07:58:26 -0300

    half finaly

--- src/components/ChallengeBox.tsx
+++ src/components/ChallengeBox.tsx
@@ -1,19 +1,21 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/ChallengeBox.module.css';
 
 export function ChallengeBox(){
-  const hasActiveChallenge = true;
+  const { activeChallenge, resetChallenge } = useContext(ChallengesContext);
 
   return (
     <div className={styles.challengeBoxContainer}>
-      { hasActiveChallenge ? (
+      { activeChallenge ? (
         <div className={styles.challengeActive}>
-          <header>Ganhe 400 xp</header>
+          <header>Ganhe {activeChallenge.amount} xp</header>
 
           <main>
-            <img src="icons/body.svg" />
+            <img src={`icons/${activeChallenge.type}.svg`} />
             <strong>Novo desafio</strong>
             <p>
-              Levante e faça uma caminha de 3 minutos.
+              {activeChallenge.description}
             </p>
           </main>
 
@@ -21,6 +23,7 @@ export function ChallengeBox(){
             <button 
               type="button"
               className={styles.challengeFailedButton}
+              onClick={resetChallenge}
             >
               Falhei
             </button>
